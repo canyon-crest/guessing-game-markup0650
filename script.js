d7cf7aeaf96ef921a5bd05bea3f6f533249e2b6a
markup0650 <kupolovm0650@my.sduhsd.net>
2026-04-10 03:29:07 +0000

    - added day suffix mapping to time()

--- script.js
+++ script.js
@@ -112,11 +112,14 @@ function reset() {
     guessCount = 0;
 }
 
+const monthMap = {0: "January", 1: "February", 2: "March", 3: "April", 4: "May", 5: "June", 6: "July", 7: "August", 8: "September", 9: "October", 10: "November", 11: "December"};
+const dayMap = {1: "1st", 2: "2nd", 3: "3rd", 4: "4th", 5: "5th", 6: "6th", 7: "7th", 8: "8th", 9: "9th", 10: "10th", 11: "11th", 12: "12th", 13: "13th", 14: "14th", 15: "15th", 16: "16th", 17: "17th", 18: "18th", 19: "19th", 20: "20th", 21: "21st", 22: "22nd", 23: "23rd", 24: "24th", 25: "25th", 26: "26th", 27: "27th", 28: "28th", 29: "29th", 30: "30th", 31: "31st"};
+
 function time() {
     const rn = new Date();
     const hours = String(rn.getHours()).padStart(2, "0");
     const minutes = String(rn.getMinutes()).padStart(2, "0");
     const seconds = String(rn.getSeconds()).padStart(2, "0");
     const ms = String(rn.getMilliseconds()).padStart(3, "0");
-    document.getElementById("date").innerHTML = `${{0: "January", 1: "February", 2: "March", 3: "April", 4: "May", 5: "June", 6: "July", 7: "August", 8: "September", 9: "October", 10: "November", 11: "December"}[rn.getMonth()]} ${rn.getDate()}, ${rn.getFullYear()}<br>${hours}:${minutes}:${seconds}.${ms}`;
+    document.getElementById("date").innerHTML = `${monthMap[rn.getMonth()]} ${dayMap[rn.getDate()]}, ${rn.getFullYear()}<br>${hours}:${minutes}:${seconds}.${ms}`;
 }
